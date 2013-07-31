fb16868e0f1a72536fe2bf1e2ddedc2475a4797e
Michae <shrapnelbikes@gmail.com>
2013-07-31 22:51:27 +1000

    fixing up line lengths

--- main.c
+++ main.c
@@ -44,16 +44,20 @@ int main(int argc, char** argv) {
     }
     initPopulation(&population, numOfPopulation, numOfCities);
 
-    int fittest = getFittest(cities, &population, numOfPopulation, numOfCities);
+    int fittest = getFittest(cities, &population, numOfPopulation, 
+            numOfCities);
     Tour eliteTour = population.tours[fittest];
-    printf("The best is: %d with a distance of %f\n", fittest, eliteTour.distance);
+    printf("The best is: %d with a distance of %f\n", fittest, 
+            eliteTour.distance);
 
     for (generation = 0; generation < maxGeneration; generation++) {
-        evolvePopulation(&population, numOfPopulation, numOfCities, eliteTour, cities, generation);
+        evolvePopulation(&population, numOfPopulation, numOfCities, eliteTour, 
+                cities, generation);
         mutatePopulation(&population, numOfPopulation, numOfCities);
         fittest = getFittest(cities, &population, numOfPopulation, numOfCities);
         eliteTour = population.tours[fittest];
-        printf("The best is: %d with a distance of %f\n", fittest, population.tours[fittest].distance);
+        printf("The best is: %d with a distance of %f\n", fittest, 
+                population.tours[fittest].distance);
     }
     return (EXIT_SUCCESS);
 }
